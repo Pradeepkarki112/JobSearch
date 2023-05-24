8a09deeac9f0d8c192879e15f43444268e7e5d56
Pradeepkarki112 <pradeepkarki112@gmail.com>
2023-05-24 07:35:40 +0545

    small changes

--- src/component/Searchbar.js
+++ src/component/Searchbar.js
@@ -4,10 +4,20 @@ import { AiFillCloseCircle } from "react-icons/ai";
 
 export const Searchbar = () => {
   return (
-    <div className="bg-[#F1F4F8] p-12 grid">
+    <div className="bg-[#F1F4F8] p-12 gap-10 rounded-xl grid">
       <form>
         <div>
-          <div className="flex bg-white rounded-[10px] gap-10 justify-between items-center shadow-lg">
+          <div className="flex bg-white rounded-[10px] gap-10 p-5 justify-between items-center shadow-lg">
+            
+            <div className="flex gap-2 items-center">
+              <AiOutlineSearch className="text-2xl cursor-pointer" />
+              <input
+                placeholder="Search Job Here..."
+                className="outline-none bg-transparent w-[100%]"
+              ></input>
+              <AiFillCloseCircle className="text-2xl text-[#676767] hover:text-[black]" />
+            </div>
+
             <div className="flex gap-2 items-center p-4">
               <AiOutlineSearch className="text-2xl cursor-pointer" />
               <input
@@ -16,6 +26,7 @@ export const Searchbar = () => {
               ></input>
               <AiFillCloseCircle className="text-2xl text-[#676767] hover:text-[black]" />
             </div>
+
             <div className="flex gap-2 items-center p-4">
               <AiOutlineSearch className="text-2xl cursor-pointer" />
               <input
@@ -24,20 +35,19 @@ export const Searchbar = () => {
               ></input>
               <AiFillCloseCircle className="text-2xl text-[#676767] hover:text-[black]" />
             </div>
-            <div className="flex gap-2 items-center p-4">
-              <AiOutlineSearch className="text-2xl cursor-pointer" />
-              <input
-                placeholder="Search Job Here..."
-                className="outline-none bg-transparent w-[100%]"
-              ></input>
-              <AiFillCloseCircle className="text-2xl text-[#676767] hover:text-[black]" />
-            </div>
+
             <div className="text-2xl cursor-pointer p-5 px-10 rounded-[10px] text-white bg-blue-600 hover:bg-blue-300 ">
               <button>Search</button>
             </div>
           </div>
         </div>
       </form>
+
+      <div className="flex items-center">
+
+      </div>
+
+
     </div>
   );
 };
